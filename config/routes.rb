2cc6baa7632b7c6d9901afdf70967b8eb01ad60c
Madalyn Wright <mwright8168@gmail.com>
2016-10-06 21:20:12 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'recipes#index'
+
 end
+
+
