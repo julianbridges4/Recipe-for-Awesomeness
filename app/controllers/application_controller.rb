1bdd57034c5db88f373cab067bd69e2f726b1b01
Madalyn Wright <mwright8168@gmail.com>
2016-10-09 21:25:51 -0400

    add details to users, update new and edit forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
+
+  before_action :configure_permitted_parameters, if :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :birthday])
+  	devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :birthday])
+  end
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010002602) do
+ActiveRecord::Schema.define(version: 20161010005702) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 20161010002602) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
+    t.string   "name"
+    t.date     "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
