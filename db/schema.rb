f71d494fde55792ad06fe0eb55de5c1640293d89
Madalyn Wright <mwright8168@gmail.com>
2016-10-09 20:43:37 -0400

    fixed the broken stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009235453) do
+ActiveRecord::Schema.define(version: 20161010002602) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20161009235453) do
     t.text     "recipe_post"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
